a6f9e07ec440f00ed5ac2f99032c839f273a2290
zabethye <dulgiq9666@gmail.com>
2022-09-06 00:15:31 +0300

    -

--- login_form.py
+++ login_form.py
@@ -1,7 +1,7 @@
 from tkinter import *
+from my_functions import database_func
+
 #Creating Main window
-
-
 login_page = Tk()
 login_page.geometry("740x370")
 login_page.title("Transport Management System v.0.1")
@@ -9,6 +9,8 @@ login_page.title("Transport Management System v.0.1")
 
 login_or_register_label = Label(login_page, text = "Log in to your account", font=('Helvetica 20 underline')).grid(row= 0, column= 1)
 
+
+###LOG_IN=============LOG_IN==============LOG_IN###
 #Putting empty lines and Log In text
 empty_line = Label(login_page, text = " ", font=5).grid(row = 1, column = 0)
 log_in_text = Label(login_page, text = "You have an account? Log In", font=('Helvetica 15 italic')).grid(row = 2, column = 0)
@@ -25,9 +27,12 @@ password = StringVar()
 password_entry = Entry(login_page, textvariable=password, show="*", font=5). grid(row=5, column=1)
 
 #Creating Log In button and forwarding to transport management system
+
 log_in_btn = Button(login_page, text="Log In", font=5).grid(row=6, column=2)
 empty_line = Label(login_page, text = " ", font=5).grid(row = 7, column = 0)
 
+
+###REGISTER=============REGISTER==============REGISTER###
 #Putting Register text
 empty_line = Label(login_page, text = " ").grid(row = 8, column = 0)
 register_text = Label(login_page, text = "Don't have an account?       \n Let's make one!", font=('Helvetica 15 italic')).grid(row=9, column=0)
